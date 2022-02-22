d37807543dd5d099aa297e46011200c3104a2faf
guipalm4 <gomes.lmc@gmail.com>
2022-02-22 20:35:26 -0300

    [REFACTORING] Aplica Mudar declaração de função (Change Function Declaration) - passo 1

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -12,6 +12,29 @@ export function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
+  function amountFor(aPerformance, play) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
     //const play = playFor(perf);
     let thisAmount = amountFor(perf, playFor(perf));
@@ -28,26 +51,5 @@ export function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(aPerformance, play) {
-  let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`Unknown type: ${play.type}`);
-  }
-  return result;
-}
 
