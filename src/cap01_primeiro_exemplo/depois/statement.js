1fa43216f8928927ff69e396e56b0ca6fc107ebd
guipalm4 <gomes.lmc@gmail.com>
2022-02-22 20:21:20 -0300

    [REFACTORING] Aplica Internalizar variável (Inline Variable)

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -13,14 +13,14 @@ export function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    //const play = playFor(perf);
+    let thisAmount = amountFor(perf, playFor(perf));
     // soma créditos por volume
     volumeCredits += Math.max(perf.audience - 30, 0);
     // soma um crédito extra para cada dez espectadores de comédia
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf)) volumeCredits += Math.floor(perf.audience / 5);
     // exibe a linha para esta requisição
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owned is ${format(totalAmount/100)}\n`
