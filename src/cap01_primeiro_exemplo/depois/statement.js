223f92d2fa7062af6474faac1888c3b3630e6cd2
guipalm4 <gomes.lmc@gmail.com>
2022-02-23 22:55:30 -0300

    [REFACTORING] Aplica Dividir laço (Split Loop)

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -1,58 +1,61 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
 
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-    // exibe a linha para esta requisição
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
-  }
-  result += `Amount owned is ${usd(totalAmount)}\n`
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+    for (let perf of invoice.performances) {
+        // exibe a linha para esta requisição
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
-    return result;
-  }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
-      result += Math.floor(aPerformance.audience / 5);
+    result += `Amount owned is ${usd(totalAmount)}\n`
+    result += `You earned ${volumeCredits} credits\n`;
     return result;
-  }
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-        {
-          style: "currency", currency: "USD",
-          minimumFractionDigits: 2
-        }).format(aNumber/100);
-  }
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
+    }
 }
