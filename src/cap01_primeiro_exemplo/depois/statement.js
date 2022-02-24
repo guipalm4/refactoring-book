ded5ce6e7d9e99c8b5bf31254969c2d53e270a77
guipalm4 <gomes.lmc@gmail.com>
2022-02-24 14:13:44 -0300

    [REFACTORING] Separar em fases (Split Phase) e Extrair Função (Extract Function)

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -1,5 +1,8 @@
 export function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
 
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
