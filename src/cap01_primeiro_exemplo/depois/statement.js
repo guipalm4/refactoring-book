c5460b6624381a8209a0b6d02f3106985a67e69e
guipalm4 <gomes.lmc@gmail.com>
2022-02-23 22:59:16 -0300

    [REFACTORING] Aplica Deslocar instruções (Slide Statements) move declaração da variavel para perto do laço

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
@@ -8,10 +7,11 @@ export function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
-
     result += `Amount owned is ${usd(totalAmount)}\n`
     result += `You earned ${volumeCredits} credits\n`;
     return result;
