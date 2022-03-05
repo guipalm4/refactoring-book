c1e035268fa18bc292eca8dc1feb360bf1cc5456
guipalm4 <gomes.lmc@gmail.com>
2022-03-04 22:36:53 -0300

    [REFACTORING] Aplica Mover função (Move function) em totalAmount() e totalVolumeCredits()

--- src/cap01_primeiro_exemplo/depois/statement.js
+++ src/cap01_primeiro_exemplo/depois/statement.js
@@ -2,6 +2,8 @@ export function statement(invoice, plays) {
     const statementData = [];
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -45,18 +47,8 @@ export function statement(invoice, plays) {
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
-}
 
-function renderPlainText(statementData) {
-    let result = `Statement for ${statementData.customer}\n`;
-    for (let perf of statementData.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owned is ${usd(totalAmount())}\n`
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
-
-    function totalAmount() {
+    function totalAmount(statementData) {
         let result = 0;
         for (let perf of statementData.performances) {
             result += perf.amount;
@@ -64,13 +56,24 @@ function renderPlainText(statementData) {
         return result;
     }
 
-    function totalVolumeCredits() {
+    function totalVolumeCredits(statementData) {
         let result = 0;
         for (let perf of statementData.performances) {
             result += perf.volumeCredits;
         }
         return result;
     }
+}
+
+function renderPlainText(statementData) {
+    let result = `Statement for ${statementData.customer}\n`;
+    for (let perf of statementData.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owned is ${usd(statementData.totalAmount)}\n`
+    result += `You earned ${statementData.totalVolumeCredits} credits\n`;
+    return result;
+
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
