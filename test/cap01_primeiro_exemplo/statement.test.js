68fb09a32323fa037bb95b2fc7ed31ef87ae8fee
guipalm4 <gomes.lmc@gmail.com>
2022-03-05 10:28:41 -0300

    [REFACTORING] Aplica Separar em fases (Split Phase), cria htmlStatement e testes unitários

--- test/cap01_primeiro_exemplo/statement.test.js
+++ test/cap01_primeiro_exemplo/statement.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {statement} from '../../src/cap01_primeiro_exemplo/depois/statement.js';
+import {htmlStatement, statement} from '../../src/cap01_primeiro_exemplo/depois/statement.js';
 import {statement as antes} from '../../src/cap01_primeiro_exemplo/antes/statement.js';
 import fs from "fs";
 
@@ -10,5 +10,18 @@ describe('statement', () => {
     it('should print a statement for multiple plays, single customer and multiple seats in plain text', () => {
         let expected = antes(invoices, plays);
         expect(statement(invoices, plays)).to.equal(expected);
+    });
+
+    it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
+        let result = `<h1>Statement for BigCo</h1>\n`;
+        result += "<table>\n";
+        result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n`;
+        result += `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n`;
+        result += `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n`;
+        result += "</table>\n";
+        result += `<p>Amount owed is <em>$1,730.00</em></p>\n`;
+        result += `<p>You earned <em>47</em> credits</p>\n`;
+
+        expect(htmlStatement(invoices, plays)).to.equal(result);
     });
 });
